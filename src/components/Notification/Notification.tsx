0e80106ccae26c152bd3001067e0f8e5f0c9913b
Diana Prihozhyna <dianapri0303@gmail.com>
2026-03-06 23:06:11 +0000

    Update Notification.tsx

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -2,8 +2,6 @@ import css from "./Notification.module.css";
 
 export default function Notification() {
   return (
-    <div className={css.container}>
       <p className={css.message}>No feedback yet</p>
-    </div>
   );
 }
